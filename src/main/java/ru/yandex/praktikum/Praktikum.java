ae6c8bcb9eb0751e7f0b791550073fa6842c5696
Alexandr Perebeynos <aperebejnos2014@yandex.ru>
2022-08-01 21:33:02 +0300

    Изменил джаву на 11

--- src/main/java/ru/yandex/praktikum/Praktikum.java
+++ src/main/java/ru/yandex/praktikum/Praktikum.java
@@ -8,6 +8,9 @@ public class Praktikum {
              Нужно создать экземпляр класса Account: в качестве аргумента передать тестируемое имя
              и вызвать метод, который проверяет, можно ли использовать фамилию и имя для печати на банковской карте.
          */
+        String nameAccount = "Виталий Миронов";
+        Account account = new Account(nameAccount);
+        System.out.println(account.checkNameToEmboss());
     }
 
 }
