054b86ce968715a2ae640d32d3e1c5d237dc8619
Alexandr Perebeynos <aperebejnos2014@yandex.ru>
2022-08-03 22:38:37 +0300

    Забыл вызвать метод проверки

--- src/main/java/ru/yandex/praktikum/Praktikum.java
+++ src/main/java/ru/yandex/praktikum/Praktikum.java
@@ -3,8 +3,9 @@ package ru.yandex.praktikum;
 public class Praktikum {
 
     public static void main(String[] args) {
-        String testName = "Тимоти Шаламе";
+        String testName = "ТимотиШаламе";
         Account account = new Account(testName);
+        account.checkNameToEmboss();
     }
 
 }
