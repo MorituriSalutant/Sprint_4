e2c67eb9182495e2661c62618d6c1f9399ffa5b5
Alexandr Perebeynos <aperebejnos2014@yandex.ru>
2022-08-03 23:12:45 +0300

    Удалил лишний класс с тестами. Написал параметризованные тесты для метода

--- src/test/java/ru/yandex/praktikum/AccountTest.java
+++ src/test/java/ru/yandex/praktikum/AccountTest.java
@@ -1,20 +1,42 @@
 package ru.yandex.praktikum;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
 
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.*;
 
+@RunWith(Parameterized.class)
 public class AccountTest {
-    @Test
-    public void checkNameToEmbossTrueTest() {
-        Account account = new Account("Шаловей Григорий");
-        assertTrue(account.checkNameToEmboss());
+
+    private final String name;
+    private final boolean expectBoolean;
+
+    public AccountTest(String name, boolean expectBoolean){
+        this.name = name;
+        this.expectBoolean = expectBoolean;
+    }
+
+    @Parameterized.Parameters
+    public static Object[][] getData(){
+        return new Object[][]{
+                {"Ша", false},
+                {"ШаловейГригорийШаловейГригорий", false},
+                {" Шаловей Григорий", false},
+                {"Шаловей Григорий ", false},
+                {" Шаловей Григорий ", false},
+                {"ШаловейГригорий", false},
+                {"Шал овейГриг орий", false},
+                {"Шаловей Григорий", true},
+        };
     }
 
     @Test
-    public void checkNameToEmbossFalseTest() {
-        Account account = new Account("ШаловейГригорий");
-        assertFalse(account.checkNameToEmboss());
+    public void checkNameToEmbossTrueTest() {
+        Account account = new Account(name);
+
+        boolean actualBoolean = account.checkNameToEmboss();
+
+        assertEquals(expectBoolean, actualBoolean);
     }
 }
